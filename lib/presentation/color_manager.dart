f362c36f8cd5557e215429b90744b9a7c264d620
Youssef Essam <113689276+Youssef-Essam-Fathy@users.noreply.github.com>
2024-03-24 17:44:13 +0200

    modifying some files

--- lib/presentation/color_manager.dart
+++ lib/presentation/color_manager.dart
@@ -5,10 +5,9 @@ class ColorManager
   static Color primary = const Color(0xffF1F1F1);
   static Color lightPrimary = const Color(0xccFFFFFF);//primary color with 80% opacity
   static Color darkPrimary = const Color(0xff303030);
-  static Color purple = const Color (0xff951B81);
-  static Color lightPurple = const Color (0xBA951B81);
-  static Color lightBlue = const Color(0xff2DAAE1);
   static Color lightGrey = const Color(0xffA8A8A8);
+  static Color maximumBlueGreen = const Color(0xff27C4B9);
+  static Color stormCloud = const Color(0xff506367);
   static Color darkBlue = const Color(0xff1877F2);
   static Color greyScale = const Color(0xffF1F1F1);
   static Color orange = const Color(0xffFFA200);
