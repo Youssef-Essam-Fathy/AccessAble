353f7217e49d9a4c6e37c3c71c17008dc2ce96cd
abdoomr2003 <abdoomer1112003@gmail.com>
2024-03-28 08:19:34 +0200

    adding firebase to sign in and sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:accessable/app/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp( MyApp());
 }
