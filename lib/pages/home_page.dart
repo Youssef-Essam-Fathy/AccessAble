8c2df93ed3b0a979f20007236e708a82df0f8425
Youssef Essam <113689276+Youssef-Essam-Fathy@users.noreply.github.com>
2024-03-28 08:32:47 +0200

    Adding first service

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,70 +1,153 @@
+import 'package:accessable/pages/service_details_page.dart';
 import 'package:flutter/material.dart';
 import 'package:accessable/presentation/color_manager.dart';
-import 'drawer_screen.dart'; // Import the DrawerScreen
+import 'drawer_screen.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: Builder(
-          builder: (context) => IconButton(
-            icon: Icon(Icons.menu, color: ColorManager.stormCloud,),
-            onPressed: () => Scaffold.of(context).openDrawer(),
-          ),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/notifications');
-            },
-            icon: Icon(Icons.notifications, color: ColorManager.stormCloud,),
-          ),
-        ],
-        title: RichText(
-          text: TextSpan(
-            children: <TextSpan>[
-              TextSpan(
-                  text: 'Access',
-                  style: TextStyle(
-                    color: ColorManager.maximumBlueGreen,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  )
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(30), // adjust the curve as needed
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Builder(
+            builder: (context) => IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: ColorManager.stormCloud,
               ),
-              TextSpan(
-                text: 'Able',
-                style: TextStyle(
-                  color: ColorManager.stormCloud,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ],
-          ),
-        ),
-        backgroundColor: ColorManager.primary,
-      ),
-      body: Column(
-        children: [
-          Card(
-            margin: EdgeInsets.all(10),
-            child: Container(
-              height: 200, // adjust the height as needed
-              child: PageView(
-                children: const [
-                  Center(child: Text('Service 1: Handicapped Transportation')),
-                  Center(child: Text('Service 2: Handicapped job opportunity')),
-                  Center(child: Text('Service 3: Handicapped communication')),
-                ],
-              ),
+              onPressed: () => Scaffold.of(context).openDrawer(),
             ),
           ),
-          // other widgets go here...
-        ],
-      ),      drawer: const DrawerScreen(), // Add this line
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/notifications');
+              },
+              icon: Icon(
+                Icons.notifications,
+                color: ColorManager.stormCloud,
+              ),
+            ),
+          ],
+          title: RichText(
+            text: TextSpan(
+              children: <TextSpan>[
+                TextSpan(
+                    text: 'Access',
+                    style: TextStyle(
+                      color: ColorManager.maximumBlueGreen,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                    )),
+                TextSpan(
+                  text: 'Able',
+                  style: TextStyle(
+                    color: ColorManager.stormCloud,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          backgroundColor: ColorManager.primary,
+        ),
+        body: Column(
+          children: [
+            Card(
+              margin: const EdgeInsets.all(10),
+              child: SizedBox(
+                height: 400, // adjust the height as needed
+                child: PageView(
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ServiceDetailsPage(
+                              imagePath: 'assets/images/h_transportation_2.svg',
+                              title: 'Handicapped Transportation',
+                              description:
+                                  'Detailed description about the service...',
+                            ),
+                          ),
+                        );
+                      },
+                      child: SvgPicture.asset(
+                        'assets/images/h_transportation_2.svg',
+                        fit: BoxFit
+                            .cover, // This will make the SVG cover the entire card
+                      ),
+                    ),
+                    const SizedBox(height: 35),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ServiceDetailsPage(
+                              imagePath: 'assets/images/h_transportation_2.jpg',
+                              title: 'Handicapped Transportation',
+                              description:
+                                  'Detailed description about the service...',
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image.asset(
+                        'assets/images/h_transportation_2.jpg',
+                        fit: BoxFit
+                            .cover, // This will make the image cover the entire card
+                      ),
+                    ),
+                    const SizedBox(height: 35),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const ServiceDetailsPage(
+                              imagePath: 'assets/images/h_transportation_2.jpg',
+                              title: 'Handicapped Transportation',
+                              description:
+                                  'Detailed description about the service...',
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image.asset(
+                        'assets/images/h_transportation_2.jpg',
+                        fit: BoxFit
+                            .cover, // This will make the image cover the entire card
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'Detailed description about the service...',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/h_transportation');
+              },
+              child: Text('Go to Handicapped Transportation'),
+            ),
+            // other widgets go here...
+          ],
+        ),
+        drawer: const DrawerScreen(), // Add this line
+      ),
     );
   }
 }
