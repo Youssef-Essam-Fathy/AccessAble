12dc10b7ea6442c61f2a5ce95e08c9c19c74c54d
Youssef Essam <113689276+Youssef-Essam-Fathy@users.noreply.github.com>
2024-03-30 22:53:52 +0200

    adding google maps and api key

--- lib/pages/handicapped_transportation_page.dart
+++ lib/pages/handicapped_transportation_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:accessable/presentation/color_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
@@ -7,78 +9,48 @@ class HandicappedTransportationPage extends StatefulWidget {
   const HandicappedTransportationPage({super.key});
 
   @override
-  _HandicappedTransportationPageState createState() => _HandicappedTransportationPageState();
+  State<HandicappedTransportationPage> createState() =>
+      _HandicappedTransportationPageState();
 }
 
-class _HandicappedTransportationPageState extends State<HandicappedTransportationPage> {
-  late GoogleMapController mapController;
-  late LatLng _center = const LatLng(0, 0);
+class _HandicappedTransportationPageState
+    extends State<HandicappedTransportationPage> {
+  final Completer<GoogleMapController> _controller =
+  Completer<GoogleMapController>();
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
 
   @override
   void initState() {
     super.initState();
-    _determinePosition();
-  }
-
-  Future<void> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    Position position = await Geolocator.getCurrentPosition();
-    _center = LatLng(position.latitude, position.longitude);
-    mapController.moveCamera(CameraUpdate.newLatLng(_center));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Handicapped Transportation'),
-        leading: IconButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          icon: Icon(Icons.arrow_back_ios_sharp,
-              color: ColorManager.stormCloud),
-        ),
-      ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 2,
-            child: GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(30), // adjust the curve as needed
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Handicapped Transportation'),
+            backgroundColor: ColorManager.primary,
+            leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(Icons.arrow_back_ios_sharp,
+                  color: ColorManager.stormCloud),
             ),
           ),
-          // Add your price display widget here
-        ],
-      ),
-    );
+          body: GoogleMap(
+            mapType: MapType.hybrid,
+            initialCameraPosition: _kGooglePlex,
+            onMapCreated: (GoogleMapController controller) {
+              _controller.complete(controller);
+            },
+          ),
+        ));
   }
 }
